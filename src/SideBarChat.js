a5517b4fc0d2be087c76f3c1e4271270d2ec637a
Namita Jadhav <2210nj@gmail.com>
2020-10-05 12:24:29 +0530

    update sidebar chat

--- src/SideBarChat.js
+++ src/SideBarChat.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import { Avatar } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 
-function SideBarChat() {
-    return (
-        <div>
-            
-        </div>
-    )
+function SideBarChat(addNewChat) {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  const createChat = () => {
+    const roomName = prompt("Please enter name for chat");
+    if (roomName) {
+      //do some db stuff
+    }
+  };
+
+  return !addNewChat ? (
+    <div className="sidebar__chat">
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+      <div className="sidebarChat__info">
+        <h2>Room Name</h2>
+        <p>Last message..</p>
+      </div>
+    </div>
+  ) : (
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add new chat</h2>
+    </div>
+  );
 }
 
-export default SideBarChat
+export default SideBarChat;
