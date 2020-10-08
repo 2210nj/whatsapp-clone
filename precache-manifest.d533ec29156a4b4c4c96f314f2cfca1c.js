485ddc502a03b95b1510036e7f9748c5a7b9e9e5
Namita Jadhav <2210nj@gmail.com>
2020-10-08 16:47:41 +0530

    Updates

---
+++ precache-manifest.d533ec29156a4b4c4c96f314f2cfca1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541e674647ec7a9be89532284a88cd53",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "c5a39cbd1278bcf23eae",
+    "url": "/whatsapp-clone/static/css/main.a1af284f.chunk.css"
+  },
+  {
+    "revision": "214bf9bca450044884e4",
+    "url": "/whatsapp-clone/static/js/2.bd8415f3.chunk.js"
+  },
+  {
+    "revision": "bd5c8e8fa25e90f36b464e8c3611da37",
+    "url": "/whatsapp-clone/static/js/2.bd8415f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a39cbd1278bcf23eae",
+    "url": "/whatsapp-clone/static/js/main.61abf5c7.chunk.js"
+  },
+  {
+    "revision": "8d0a8cdab6927a1f4f08",
+    "url": "/whatsapp-clone/static/js/runtime-main.a04c35f0.js"
+  }
+]);
